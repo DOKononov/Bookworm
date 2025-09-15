011af436a61066ae645783dba8c1f550b2a33768
d.o.kononov <d.o.kononov@gmail.com>
2025-09-15 09:22:27 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Dmitry Kononov on 15.09.25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
