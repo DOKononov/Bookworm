98ad4e0911d134eca9a03998bd867fd4c6c9d3cc
d.o.kononov <d.o.kononov@gmail.com>
2025-09-15 11:14:22 +0300

    Students/SwiftData

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -6,17 +6,33 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    
-    @AppStorage("notes") private var notes = ""
+    @Environment(\.modelContext) private var modelContext
+    @Query private var students: [Student]
     
     var body: some View {
         NavigationStack {
-            TextEditor(text: $notes)
-                .navigationTitle("Notes")
-                .padding()
+            List(students) { student in
+                Text(student.name)
+            }
+            .navigationTitle("Classroom")
+            .toolbar {
+                Button("Add") {
+                    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                    let lastNames = ["Grenger", "Lovegood", "Potter", "Weasley"]
+                    
+                    let chosenFirstName = firstNames.randomElement()!
+                    let chosenLastName = lastNames.randomElement()!
+                    
+                    let newStudent = Student(id: UUID(), name: "\(chosenFirstName) \(chosenLastName)")
+                    
+                    modelContext.insert(newStudent)
+                }
+            }
         }
+      
     }
 }
 
