602835b620f4dbb649e9adcbf4bdce581ee89b0c
d.o.kononov <d.o.kononov@gmail.com>
2025-09-16 12:42:24 +0300

    add book view

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Dmitry Kononov on 16.09.25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
