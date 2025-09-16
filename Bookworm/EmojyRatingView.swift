8b64346d63de9ada057f11d86d8993e51c6a8853
d.o.kononov <d.o.kononov@gmail.com>
2025-09-16 10:42:13 +0300

    add ratingView and BookNavCell

---
+++ Bookworm/EmojyRatingView.swift
@@ -0,0 +1,18 @@
+//
+//  EmojyRatingView.swift
+//  Bookworm
+//
+//  Created by Dmitry Kononov on 16.09.25.
+//
+
+import SwiftUI
+
+struct EmojyRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojyRatingView()
+}
